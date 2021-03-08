e85b0a5cfb3722a97a62ee1f0cc57a33ac7d18a5
Jason <getbest@gmail.com>
2021-03-08 14:21:39 -0500

    commit for checking JSON access

---
+++ index.js
@@ -0,0 +1,31 @@
+loadJson();
+
+function loadJson() {
+  fetch("./JSON/states.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      stateInfo = data;
+      loadState();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function loadState() {
+  const stateMain = document.querySelector(".stateForm");
+  const selectElmt = document.querySelector("#searchState");
+  stateMain.appendChild(selectElmt);
+  let optionElmt;
+  for (let i = 0; i < Object.keys(stateInfo).length; i++) {
+    optionElmt = document.createElement("option");
+    optionElmt.textContent = `${Object.keys(stateInfo)[i]}`;
+    selectElmt.appendChild(optionElmt);
+  }
+
+  const submitBtn = document.createElement("input");
+  submitBtn.setAttribute("type", "submit");
+  stateMain.appendChild(submitBtn);
+}
